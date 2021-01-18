08b633eb582c6ef7cff12a96145a4dc0bbd2b377
limon4ikas <limon4ikas@gmail.com>
2021-01-18 16:00:55 +0300

    handling hash changes

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,16 +30,19 @@ const renderSpinner = function (parentEl) {
 };
 
 const showRecipe = async function () {
-  // 1) Loading recipe
   try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
+    // 1) Loading recipe
     renderSpinner(recipeContainer);
     const response = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await response.json();
 
     if (!response.ok) throw new Error(`${data.message} (${response.status})`);
-    // console.log(response, data);
+
     let { recipe } = data.data;
     recipe = {
       id: recipe.id,
@@ -159,4 +162,4 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
