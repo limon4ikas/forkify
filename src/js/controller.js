178d3affc92cc382f06cca2819accc768f0c9288
limon4ikas <limon4ikas@gmail.com>
2021-01-19 13:33:07 +0300

    updating servings

--- src/js/controller.js
+++ src/js/controller.js
@@ -9,7 +9,9 @@ import paginationView from './views/paginationView.js';
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
+
     if (!id) return;
+
     recipeView.renderSpinner();
 
     // 1) Loading recipe
@@ -51,8 +53,17 @@ const controlPagination = function (goToPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function (newServings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  recipeView.render(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
